92e326c5175d26e68fd361bad98aae3957e65c7e
hoang-kim-duc <hoangkimduclqd@gmail.com>
2021-10-19 23:28:23 +0700

    User authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
+  devise_for :users, skip: :all
+  namespace :api, defaults: {format: :json} do
+    devise_scope :user do
+      post "sign_up", to: "registrations#create"
+      post "log_in", to: "sessions#create"
+    end
+  end
 end
